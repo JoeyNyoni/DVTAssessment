e0f2c53259f04d2da5f56700c1d09fcf93f295d5
Joe Nyoni <juniornyoni@gmail.com>
2022-10-16 11:22:34 +0200

    Default routing to home component, add API base Url to environment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+export const Navigation = {
+  Home: 'home'
+}
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: Navigation.Home, component: HomeComponent },
+  { path: '*', pathMatch: 'full', redirectTo: Navigation.Home }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
